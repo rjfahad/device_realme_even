1157f3a8bca8e7470be901128b6ea9086a4412fe
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-04-26 00:48:07 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,8 +7,11 @@ rm -rf ./vendor/realme/RMX3191
 rm -rf ./vendor/realme/even
 rm -rf ./device/realme/even
 rm -rf ./hardware/mediatek/InCallService
+rm -rf ./device/realme/RMX3191-kernel
 echo "succesfully"
 
 echo "clone vt"
 git clone https://github.com/cumaRull/vendor_realme_RMX3191.git -b lineage-20.0 ./vendor/realme/RMX3191
 echo "successfully"
+echo "clone Kernel prebuilt"
+git clone --depth=1 https://github.com/cumaRull/kernel_realme_RMX3191-prebuilt.git -b rui3-dev ./device/realme/RMX3191-kernel
