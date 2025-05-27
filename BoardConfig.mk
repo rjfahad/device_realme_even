da104966044be487db8015a4e488c11a8033ad36
cumaRull <rulsmods@gmail.com>
2025-05-27 11:36:34 +0700

    RMX3191: add clang flags
    Signed-off-by: cumaRull <rulsmods@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,8 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/realme/RMX3191
 TARGET_KERNEL_CONFIG := even_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS=" -fuse-ld=lld -Wno-unused-command-line-argument"
+TARGET_KERNEL_LLVM_BINUTILS := false
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/mylitle-clang
 
 BOARD_MKBOOTIMG_ARGS += --base $(BOARD_KERNEL_BASE)
