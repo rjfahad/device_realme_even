769f09c9eadb81c8c46d271ccc5186b19dbf865f
cumaRull <rulsmods@gmail.com>
2025-05-28 11:08:14 +0700

    RMX3191: add dtbo image to release
    Signed-off-by: cumaRull <rulsmods@gmail.com>

--- releasetools.py
+++ releasetools.py
@@ -39,6 +39,7 @@ def AddImage(info, basename, dest):
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching boot and vbmeta images...")
+  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta")
   AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system")
   return
