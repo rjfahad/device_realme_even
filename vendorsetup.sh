3e51cd35c16909b90577d0afa9a69fbaa6368b13
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-06-05 16:08:48 +0000

    RMX3191: drop realme dirac
    * Fuck Realme dirac for prebuilt vendor
    
    Signed-off-by: CumaRull <102359107+cumaRull@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,6 +17,5 @@ git clone https://github.com/cumaRull/vendor_realme_RMX3191.git -b twelve-rui2 .
 git clone https://github.com/cumaRull/vendor_realme_RMX3191-ims.git -b twelve ./vendor/realme/RMX3191-ims
 git clone https://github.com/Realme-C25-Series-Development/android_packages_apps_RealmeParts.git -b aosp12 ./packages/apps/RealmeParts
 git clone https://gitlab.com/MindTheGapps/vendor_gapps.git -b sigma ./vendor/gapps
-git clone https://github.com/techyminati/android_packages_apps_RealmeDirac.git -b lineage-19.1 ./packages/apps/RealmeDirac
 echo "Done!"
 
