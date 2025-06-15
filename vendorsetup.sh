e1d47d324adc5bcc7e2c4bfa55bed2a0e704158e
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-06-16 00:20:02 +0700

    RMX3191: bring up Devices

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,9 +10,10 @@ rm -rf packages/apps/RealmeParts
 rm -rf device/mediatek/sepolicy_vndr
 echo "Cloning Dependencies"
 git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b rui2 ./kernel/realme/RMX3191
+git clone --depth=1 https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-19.1 ./packages/apps/RealmeParts
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b oss-rui2-dev ./vendor/realme/RMX3191
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191-ims.git -b twelve ./vendor/realme/RMX3191-ims
-git clone --depth=1 https://github.com/cumaRull/android_device_mediatek_sepolicy_vndr.git -b lineage-19.1 ./device/mediatek/sepolicy_vndr
+git clone --depth=1 https://github.com/ArrowOS/android_device_mediatek_sepolicy_vndr.git -b arrow-12.1 ./device/mediatek/sepolicy_vndr
 echo "Done!"
 
