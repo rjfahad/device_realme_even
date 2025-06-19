ee1a7af24c49803dd9498e21e0794e5c76acd65a
Cuma.Rull Mods <rulsmods@gmail.com>
2025-06-19 12:07:40 +0000

    RMX3191: BT
    Signed-off-by: Cuma.Rull Mods <rulsmods@gmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,11 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0.vendor \
+    android.hardware.bluetooth@1.1.vendor \
+    android.hardware.bluetooth.audio-impl
 
 # fastbootd
 PRODUCT_PACKAGES += \
