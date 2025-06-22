6763639245dd84a22dffc3cd6f3b5bd88096a6fd
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-06-22 09:52:38 +0000

    RMX3191: Bringup
    Signed-off-by: CumaRull <102359107+cumaRull@users.noreply.github.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,14 +10,17 @@ rm -rf vendor/realme/RMX3191-ims
 rm -rf packages/apps/RealmeParts
 rm -rf device/mediatek/sepolicy_vndr
 rm -rf hardware/mediatek
+rm -rf prebuilts/clang/host/linux-x86/mylitle-clang
 echo "Cloning Dependencies"
 git clone https://github.com/cumaRull/kernel_realme_RMX3191-prebuilt.git -b RUI2-OSS ./device/realme/RMX3191-kernel
 git clone --depth=1 https://github.com/LineageOS/android_hardware_mediatek.git -b lineage-20 ./hardware/mediatek
-git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b rui2 ./kernel/realme/RMX3191
+git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b suki-dev ./kernel/realme/RMX3191
 git clone --depth=1 https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-20 ./packages/apps/RealmeParts
-git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b main ./prebuilts/clang/host/linux-x86/mylitle-clang
+git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b thirteen-rui2-oss ./vendor/realme/RMX3191
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191-ims.git -b thirteen ./vendor/realme/RMX3191-ims
 git clone --depth=1 https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git -b lineage-20 ./device/mediatek/sepolicy_vndr
+rm -rf ./device/realme/RMX3191-kernel/Android.bp
+rm -rf ./device/realme/RMX3191-kernel/kernel-headers
 echo "Done!"
 
