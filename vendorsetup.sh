356609dbb3ab240e88574308a0890ba41dc5a1c2
cumaRull <rulsmods@gmail.com>
2025-06-28 15:28:32 +0700

    RMX3191: droped prebuilt kernel
    Signed-off-by: cumaRull <rulsmods@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,7 +18,6 @@ echo "clone vt"
 git clone https://github.com/cumaRull/vendor_realme_RMX3191.git -b lineage-20.0 ./vendor/realme/RMX3191
 echo "successfully"
 echo "clone Kernel prebuilt"
-git clone https://github.com/cumaRull/kernel_realme_RMX3191-prebuilt.git -b master ./device/realme/RMX3191-kernel
 git clone https://github.com/kdrag0n/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_even_4.19.git -b main ./kernel/realme/RMX3191
 git clone https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-20 ./packages/apps/RealmeParts
