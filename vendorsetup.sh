f6248bcb909023b2d78dd888bc58ba4abee9f460
cumaRu <rulsmods@gmail.com>
2025-06-28 01:54:23 +0000

    RMX3191: using zyc clang
    Signed-off-by: cumaRu <rulsmods@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ rm -rf packages/apps/RealmeDirac
 
 echo "Cloning Dependencies"
 git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b suki-dev ./kernel/realme/RMX3191
-git clone --depth=1 https://github.com/picasso09/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
+git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b twelve-rui2 ./vendor/realme/RMX3191
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191-ims.git -b twelve ./vendor/realme/RMX3191-ims
 git clone --depth=1 https://github.com/Realme-C25-Series-Development/android_packages_apps_RealmeParts.git -b aosp12 ./packages/apps/RealmeParts
