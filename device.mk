ed597ebf1c5005e030d266f3f6d7d75cb5370c09
cumaRull <rulsmods@gmail.com>
2025-07-17 16:54:05 +0000

    RMX3191: Fix for build los21
    Signed-off-by: cumaRull <rulsmods@gmail.com>

--- device.mk
+++ device.mk
@@ -43,7 +43,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio_policy.stub \
-    audio.bluetooth.default \
+#    audio.bluetooth.default \
     audio.r_submix.default \
     audio.usb.default \
 
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshim_audio \
     libshim_camera_metadata \
-    libshim_mtkcam \
+#    libshim_mtkcam \
     libshim_utils
 
 # Ramdisk
