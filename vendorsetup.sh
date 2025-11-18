01f3038e4447d609036792ada6879360890c4acc
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-11-18 13:39:32 +0700

    update kernel packages

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ rm -rf vendor/realme/RMX3191
 rm -rf vendor/realme/RMX3191-ims
 rm -rf packages/apps/RealmeParts
 echo "Cloning Dependencies"
-git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b suki-dev ./kernel/realme/RMX3191
+git clone --depth=1 --recurse-submodule https://github.com/cumaRull/kernel_realme_RMX3191.git -b rui2-backupx ./kernel/realme/RMX3191
 git clone --depth=1 https://gitlab.com/clangsantoni/zyc_clang.git -b 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191.git -b twelve-rui2 ./vendor/realme/RMX3191
 git clone --depth=1 https://github.com/cumaRull/vendor_realme_RMX3191-ims.git -b thirteen ./vendor/realme/RMX3191-ims
