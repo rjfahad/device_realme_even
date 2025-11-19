3be48bff1a6215259c75127a90fb63639683bd65
CumaRull <102359107+cumaRull@users.noreply.github.com>
2025-11-19 07:49:16 +0700

    kernul

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -19,7 +19,7 @@ git clone https://github.com/cumaRull/vendor_realme_RMX3191.git -b lineage-20.0 
 echo "successfully"
 echo "clone Kernel prebuilt"
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master ./prebuilts/clang/host/linux-x86/mylitle-clang
-git clone --recurse-submodules https://github.com/Badmaneers/kernel_realme_even.git --depth=1 kernel/realme/even
+git clone --recurse-submodules https://github.com/Badmaneers/kernel_realme_even.git --depth=1 kernel/realme/RMX3191
 #git clone https://github.com/HyperTeam/android_packages_apps_RealmeParts.git -b lineage-20 ./packages/apps/RealmeParts
 git clone https://github.com/LineageOS/android_hardware_oplus.git -b lineage-22.1 ./hardware/oplus
 git clone https://github.com/cumaRull/android_hardware_mediatek.git -b lineage-22.1 ./hardware/mediatek
