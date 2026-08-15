8d86d0f61680b6bf08741eee7bc0090f072d0cc3
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-08-15 23:44:34 +0600

    even: switch kernel source to kernel/realme/even (rjfahad/kernel_realme_even rui2-backupx)
    - TARGET_KERNEL_SOURCE -> kernel/realme/even (renamed from RMX3191)
    - vendorsetup.sh: clone rjfahad/kernel_realme_even rui2-backupx

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,7 +12,7 @@ clone_if_missing() {
 echo "Checking dependencies"
 
 clone_if_missing https://github.com/LineageOS/android_hardware_mediatek.git lineage-20 ./hardware/mediatek
-clone_if_missing https://github.com/cumaRull/kernel_realme_RMX3191.git rui2-backupx ./kernel/realme/RMX3191
+clone_if_missing https://github.com/rjfahad/kernel_realme_even.git rui2-backupx ./kernel/realme/even
 clone_if_missing https://github.com/HyperTeam/android_packages_apps_RealmeParts.git lineage-20 ./packages/apps/RealmeParts
 clone_if_missing https://gitlab.com/clangsantoni/zyc_clang.git 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 clone_if_missing https://github.com/rjfahad/vendor_realme_RMX3191-ims.git thirteen ./vendor/realme/RMX3191-ims
