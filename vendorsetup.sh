a9ad2231f885abf65e9d445939747e1cf0a2e999
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-08-26 18:21:57 +0600

    vendorsetup.sh: update repository branches to lineage-21

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,10 +11,12 @@ clone_if_missing() {
 
 echo "Checking dependencies"
 
-clone_if_missing https://github.com/LineageOS/android_hardware_mediatek.git lineage-20 ./hardware/mediatek
-clone_if_missing https://github.com/rjfahad/kernel_realme_even.git rui2-backupx ./kernel/realme/even
-clone_if_missing https://github.com/HyperTeam/android_packages_apps_RealmeParts.git lineage-20 ./packages/apps/RealmeParts
-clone_if_missing https://gitlab.com/clangsantoni/zyc_clang.git 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
-clone_if_missing https://github.com/rjfahad/vendor_realme_RMX3191-ims.git thirteen ./vendor/realme/RMX3191-ims
-clone_if_missing https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git lineage-20 ./device/mediatek/sepolicy_vndr
+clone_if_missing https://github.com/rjfahad/kernel_realme_even.git los-21 ./kernel/realme/even
+clone_if_missing https://github.com/rjfahad/vendor_realme_RMX3191-ims.git fourteen ./vendor/realme/RMX3191-ims
+clone_if_missing https://github.com/HyperTeam/android_packages_apps_RealmeParts.git lineage-21 ./packages/apps/RealmeParts
+clone_if_missing https://github.com/nishant6342/packages_apps_PocketMode.git UNO ./packages/apps/PocketMode
+clone_if_missing https://github.com/LineageOS/android_hardware_mediatek.git lineage-21 ./hardware/mediatek
+clone_if_missing https://github.com/LineageOS/android_hardware_oplus.git lineage-21 ./hardware/oplus
+clone_if_missing https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git lineage-21 ./device/mediatek/sepolicy_vndr
+
 echo "Done!"
