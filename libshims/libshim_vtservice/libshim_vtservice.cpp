313aaeaf0ba62ec8482c1852edc46a8499963ea4
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-08-29 06:26:31 +0000

    libshim_vtservice: fix AudioTrack linker error and remove duplicate supplicant manifest entry
    - Replace mangled forward declaration with placement new to call the
      current AudioTrack constructor directly, fixing undefined symbol error
      caused by legacy_callback_t changing to wp<IAudioTrackCallback>.
    
    - Remove duplicate android.hardware.wifi.supplicant HIDL entry from
      device manifest that conflicts with the vendor proprietary blob's
      manifest fragment (both declare @1.3).

--- libshims/libshim_vtservice/libshim_vtservice.cpp
+++ libshims/libshim_vtservice/libshim_vtservice.cpp
@@ -5,25 +5,6 @@
 using namespace android;
 
 extern "C" {
-void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
-        void* thisptr,
-        audio_stream_type_t streamType,
-        uint32_t sampleRate,
-        audio_format_t format,
-        audio_channel_mask_t channelMask,
-        size_t frameCount,
-        audio_output_flags_t flags,
-        android::AudioTrack::legacy_callback_t cbf,
-        void* user,
-        int32_t notificationFrames,
-        audio_session_t sessionId,
-        android::AudioTrack::transfer_type transferType,
-        const audio_offload_info_t *offloadInfo,
-        const AttributionSourceState& attributionSource,
-        const audio_attributes_t* pAttributes,
-        bool doNotReconnect,
-        float maxRequiredSpeed,
-        audio_port_handle_t selectedDeviceId);
 
 void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_output_flags_tPFviPvS4_ES4_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tjiPK18audio_attributes_tbfi(
         void* thisptr,
@@ -51,10 +32,12 @@ void _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_tjj20audio_o
     attributionSource.packageName = "com.mediatek.ims";
     attributionSource.token = android::sp<android::BBinder>::make();
     attributionSource.attributionTag = std::nullopt;
-    _ZN7android10AudioTrackC1E19audio_stream_type_tj14audio_format_t20audio_channel_mask_tj20audio_output_flags_tPFviPvS5_ES5_i15audio_session_tNS0_13transfer_typeEPK20audio_offload_info_tRKNS_7content22AttributionSourceStateEPK18audio_attributes_tbfi(
-                            thisptr, streamType, sampleRate, format, channelMask, frameCount, flags, cbf,
-                            user, notificationFrames, sessionId, transferType, offloadInfo,
-                            attributionSource, pAttributes, doNotReconnect, maxRequiredSpeed, selectedDeviceId);
+    new (thisptr) AudioTrack(
+                        streamType, sampleRate, format, channelMask, frameCount, flags,
+                        wp<AudioTrack::IAudioTrackCallback>(),
+                        notificationFrames, sessionId, transferType, offloadInfo,
+                        attributionSource, pAttributes, doNotReconnect, maxRequiredSpeed,
+                        selectedDeviceId);
 }
 
 void _ZN7android7SurfaceC1ERKNS_2spINS_22IGraphicBufferProducerEEEbRKNS1_INS_7IBinderEEE(
