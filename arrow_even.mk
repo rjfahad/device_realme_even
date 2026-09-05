f22b351bde71e4e1485561423b07c4f0d595985f
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-09-06 03:44:09 +0600

    Fix ArrowOS vendor config path: common_full_phone.mk -> common.mk

--- arrow_even.mk
+++ arrow_even.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/even/device.mk)
 
 # Inherit some common ArrowOS stuff.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := arrow_even
