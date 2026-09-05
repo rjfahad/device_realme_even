2e70bdd2e4e4df867a19fa322868b48ff3e06196
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-09-06 03:43:37 +0600

    Fix crdroid vendor config path: vendor/crdroid -> vendor/lineage

--- crdroid_even.mk
+++ crdroid_even.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/even/device.mk)
 
 # Inherit some common crDroid stuff.
-$(call inherit-product, vendor/crdroid/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := crdroid_even
