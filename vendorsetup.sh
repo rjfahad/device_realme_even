dcf25517942f179c3b5a0e62dad0faa97f97f195
Alamin <asffghjjkk1971bangladesh@gmail.com>
2026-09-06 03:29:33 +0600

    Fix vendorsetup.sh: kernel los-20, RealmeParts rjfahad/lineage-20-fps

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,8 +12,8 @@ clone_if_missing() {
 echo "Checking dependencies"
 
 clone_if_missing https://github.com/LineageOS/android_hardware_mediatek.git lineage-20 ./hardware/mediatek
-clone_if_missing https://github.com/rjfahad/kernel_realme_even.git rui2-backupx ./kernel/realme/even
-clone_if_missing https://github.com/HyperTeam/android_packages_apps_RealmeParts.git lineage-20 ./packages/apps/RealmeParts
+clone_if_missing https://github.com/rjfahad/kernel_realme_even.git los-20 ./kernel/realme/even
+clone_if_missing https://github.com/rjfahad/android_packages_apps_RealmeParts.git lineage-20-fps ./packages/apps/RealmeParts
 clone_if_missing https://gitlab.com/clangsantoni/zyc_clang.git 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
 clone_if_missing https://github.com/rjfahad/vendor_realme_RMX3191-ims.git thirteen ./vendor/realme/RMX3191-ims
 clone_if_missing https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git lineage-20 ./device/mediatek/sepolicy_vndr
