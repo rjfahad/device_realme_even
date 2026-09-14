ac4740082a9b628cf9bca892f7b2eb5163ecc589
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-09-15 01:04:54 +0600

    vendorsetup: zyc_clang → Greenforce Clang 24

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,7 +14,7 @@ echo "Checking dependencies"
 clone_if_missing https://github.com/LineageOS/android_hardware_mediatek.git lineage-20 ./hardware/mediatek
 clone_if_missing https://github.com/rjfahad/kernel_realme_even.git rui2-backupx ./kernel/realme/even
 clone_if_missing https://github.com/HyperTeam/android_packages_apps_RealmeParts.git lineage-20 ./packages/apps/RealmeParts
-clone_if_missing https://gitlab.com/clangsantoni/zyc_clang.git 14 ./prebuilts/clang/host/linux-x86/mylitle-clang
+clone_if_missing https://github.com/greenforce-project/greenforce_clang.git main ./prebuilts/clang/host/linux-x86/greenforce-clang
 clone_if_missing https://github.com/rjfahad/vendor_realme_RMX3191-ims.git thirteen ./vendor/realme/RMX3191-ims
 clone_if_missing https://github.com/LineageOS/android_device_mediatek_sepolicy_vndr.git lineage-20 ./device/mediatek/sepolicy_vndr
 echo "Done!"
