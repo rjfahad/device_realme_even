039eca4232c411b9d70e5f3e648bb0722d2e6995
rjfahad <asffghjjkk1971bangladesh@gmail.com>
2026-09-19 01:14:53 +0600

    BoardConfig: fix kernel clang path (mylitle-clang -> greenforce-clang)

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -114,7 +114,7 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/realme/even
 TARGET_KERNEL_CONFIG := RMX3191_defconfig
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/mylitle-clang
+TARGET_KERNEL_CLANG_PATH := $(abspath prebuilts/clang/host/linux-x86/greenforce-clang)
 BOARD_KERNEL_IMAGE_NAME := Image.gz
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS=" -fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_LLVM_BINUTILS := false
